4abe4812dd12050d1408844c93c1bb53cf36babf
Jesse Howell <147555735+jesse-howell@users.noreply.github.com>
2024-01-06 20:58:53 -0500

    added first question

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = inquirer.createPromptModule();
+
+questions ([
+    {
+        type: 'input',
+        message: 'Please enter your project name.',
+        name: 'name',
+        default: 'Your-Project-Title'
+    },
+]);
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
