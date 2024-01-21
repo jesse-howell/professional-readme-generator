5439f5c3b6f153a75879267812c3a3b70b614c9a
Jesse Howell <147555735+jesse-howell@users.noreply.github.com>
2024-01-21 18:01:33 -0500

    added question array

--- index.js
+++ index.js
@@ -4,7 +4,61 @@ const fs = require('fs');
 const inquirer = require('inquirer');
 const generateMarkdown = require('./utils/generateMarkdown');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = inquirer.createPromptModule;
+
+questions ([
+{
+    type: 'input',
+    message: 'What is your project title?',
+    name: 'title',
+},
+
+{
+    type: 'input',
+    message: 'What is a description of your project? (Your motivation, why you built this, what problem this solves and what you learned.)',
+    name: 'description',
+},
+{
+    type: 'input',
+    message: 'What is your table of contents for this readme?',
+    name: 'table of contents',
+},
+{
+    type: 'input',
+    message: 'How do you install this project? (step-by-step)',
+    name: 'installation',
+},
+{
+    type: 'input',
+    message: 'How do you use this project? Include screenshots as needed.',
+    name: 'usage',
+},
+{
+    type: 'input',
+    message: 'Can you provide links to your collaborators, third-party assets or tutorials used?',
+    name: 'credits',
+},
+{
+    type: 'input',
+    message: 'Does this project have any features?',
+    name: 'features',
+},
+{
+    type: 'input',
+    message: 'How would someone contribute if they could?',
+    name: 'contribute',
+},
+{
+    type: 'input',
+    message: 'Can you provide some tests for your application?',
+    name: 'tests',
+},
+{
+    type: 'input',
+    message: 'How can you be contacted if need be?',
+    name: 'questions',
+},
+])
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
